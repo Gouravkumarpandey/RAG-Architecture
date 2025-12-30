96c8cbe32cd8c07a304e842bfca6ac5a1531a15f
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-12-30 23:53:39 +0530

    chore: ignore node_modules; fix worker import

--- server/worker.js
+++ server/worker.js
@@ -1,8 +1,32 @@
 import { Worker } from 'bullmq';
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { Document } from '@langchain/core/documents';
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { CharacterTextSplitter } from "@langchain/textsplitters";
+
 
 const worker = new Worker('file-upload-queue', 
     async (job) => {
-        console.log(`Job:`, job,data);
+        console.log(`Job:`, job.data);
+        const data = JSON.parse(job.data)
+        /*
+        Path: data.path
+        read the pdf from path,
+        call the openai embedding model for every chunk,
+        store the chunk in qdrant db
+
+        */
+       //Load the PDF document
+       const loader = new PDFLoader(data.path);
+       const docs = await loader.load();
+
+       const textSplitter = new CharacterTextSplitters({
+        chunkSize: 300,
+        chunkOverlap: 0,
+       });
+       const texts = await textSplitter.splitText(document);
+       console.log(texts);
     },
     { concurrency: 100, connection: { host: 'localhost', port: 6379 }   
     },
